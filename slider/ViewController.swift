27ce16470f6f254f94815d398f8f73448f373f51
Student 06 <student06@apples-macbook-air.local>
2018-08-23 14:59:00 +0530

    Initial Commit

---
+++ slider/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  slider
+//
+//  Created by Student 06 on 23/08/18.
+//  Copyright © 2018 Student 06. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
